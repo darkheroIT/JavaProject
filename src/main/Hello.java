a42319d01b20f91763d2e72f757624c5dc33c328
DarkHeroIT <vuong.phan1989@gmail.com>
2021-09-27 10:06:35 +0700

    added HelloWorld program

---
+++ src/main/Hello.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created by vuongphan on 09/27/2021 - - 09:16
+ *
+ * @project JavaProject
+ */
+public class Hello {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("main.Hello World ..." + i);
+        }
+    }
+}
